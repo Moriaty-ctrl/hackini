1ca0d97b0de93726554642c6ecb27fc97e64a97c
Moriaty-ctrl <a_merzougui@estin.dz>
2025-03-11 03:05:17 +0100

    Set up the profile theme

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCy8Me6lEYmaeKfNkOr16BJ17_XOhp2GJc",
     authDomain: "hackini-394d1.firebaseapp.com",
     projectId: "hackini-394d1",
-    storageBucket: "hackini-394d1.firebasestorage.app",
+    storageBucket: "hackini-394d1.appspot.com",
     messagingSenderId: "749500241115",
     appId: "1:749500241115:web:25ffd9499223be923a68fe",
     measurementId: "G-7VZS03ZGN5"
